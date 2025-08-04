148802eda7f13d9221443b8825d444492844a9f7
sophianx234 <dx4336969@gmail.com>
2025-08-04 13:27:19 +0000

    feat: add location feature

--- src/app/_components/LocationMarker.tsx
+++ src/app/_components/LocationMarker.tsx
@@ -1,26 +1,32 @@
-
+// LocationMarker.tsx
 'use client'
+import { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
 
-import { useState } from 'react'
-import { Marker, Popup, useMapEvents } from 'react-leaflet'
-import { LatLng } from 'leaflet'
+export function LocationMarker({ trigger }: { trigger: boolean }) {
+  const [position, setPosition] = useState(null);
+  const map = useMap();
 
-export function LocationMarker() {
-  const [position, setPosition] = useState<LatLng | null>(null)
+  useEffect(() => {
+    if (!trigger) return;
 
-  const map = useMapEvents({
-    click() {
-      map.locate()
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
-    },
-  })
+    map.locate();
 
-  return position === null ? null : (
+    function onLocationFound(e: any) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    }
+
+    map.on("locationfound", onLocationFound);
+
+    return () => {
+      map.off("locationfound", onLocationFound);
+    };
+  }, [trigger, map]);
+
+  return position ? (
     <Marker position={position}>
       <Popup>You are here</Popup>
     </Marker>
-  )
+  ) : null;
 }
--- src/app/_components/Map.tsx
+++ src/app/_components/Map.tsx
@@ -9,6 +9,8 @@ import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { MdOutlineGpsFixed } from "react-icons/md";
+import { useState } from "react";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -20,11 +22,11 @@ L.Icon.Default.mergeOptions({
 
 export  function Map() {
   const {accidents } = useDashStore()
-
-
+  const [useLocation, setUseLocation] = useState(false);
+  console.log('use location', useLocation )
   return (
     <div className="p-2 border    row-span-1   border-gray-200 rounded-lg shadow">
-      <div className="size-full   rounded-xl border-gray-500 overflow-hidden">
+      <div className="size-full relative   rounded-xl border-gray-500 overflow-hidden">
         <MapContainer
           center={[7.9465, -1.0232]}
           zoom={6}
@@ -32,8 +34,11 @@ export  function Map() {
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           
-              <LocationMarker/>
+              <LocationMarker trigger={useLocation}/>
         </MapContainer>
+      <div onClick={()=>setUseLocation(state=>!state)} className="rounded-full p-1 right-2 absolute bottom-2 hover:scale-110 duration-150 border border-gray-200  bg-white z-50 shadow">
+        <MdOutlineGpsFixed className="size-5 text-gray-500  "/>
+        </div>
       </div>
     </div>
   );
